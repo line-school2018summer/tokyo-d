8fd728e85e4da071cb09a8f2ef2f2cfcb894cbad
proelbtn <proelbtn@users.noreply.github.com>
2018-08-10 18:06:30 +0900

    [ADD] spring initializrのソースコードを追加した。

---
+++ backend/src/main/kotlin/com/proelbtn/linesc/LineScApplication.kt
@@ -0,0 +1,11 @@
+package com.proelbtn.linesc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LineScApplication
+
+fun main(args: Array<String>) {
+    runApplication<LineScApplication>(*args)
+}
