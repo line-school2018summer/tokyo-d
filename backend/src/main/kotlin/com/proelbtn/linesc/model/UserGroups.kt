ab016f252f5a24d3355f28be9f174a0b7dd1c739
proelbtn <proelbtn@users.noreply.github.com>
2018-08-15 17:42:40 +0900

    [ADD] グループ作成者を定義した。

--- backend/src/main/kotlin/com/proelbtn/linesc/model/UserGroups.kt
+++ backend/src/main/kotlin/com/proelbtn/linesc/model/UserGroups.kt
@@ -6,6 +6,7 @@ object UserGroups: Table() {
     val id = uuid("id").primaryKey()
     val sid = varchar("sid", 32).uniqueIndex()
     val name = varchar("name", 32)
+    val owner = (uuid("owner") references Users.id)
     val createdAt = UserMessages.datetime("created_at")
     val updatedAt = UserMessages.datetime("updated_at")
 }
