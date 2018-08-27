e64f71ad6831b0eaca7069b9148b3d5223edfd69
proelbtn <proelbtn@users.noreply.github.com>
2018-08-28 00:58:51 +0900

    [ADD] APIの自動生成ができるようになった。
    UIの調整はあとでやるとする。

--- backend/src/main/kotlin/com/proelbtn/linesc/LineScApplication.kt
+++ backend/src/main/kotlin/com/proelbtn/linesc/LineScApplication.kt
@@ -10,11 +10,26 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.handler.MappedInterceptor
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 
 @Configuration
 @SpringBootApplication
+@EnableSwagger2
 class LineScApplication {
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+    }
+
     @Bean
     fun interceptor(): MappedInterceptor {
         return MappedInterceptor(arrayOf("/**"), AuthorizationInterceptor())
