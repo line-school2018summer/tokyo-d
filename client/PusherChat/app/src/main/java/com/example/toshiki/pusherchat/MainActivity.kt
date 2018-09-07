c46207279370f9cead37d8826ce7ea44e0d770be
ti-ginkgo <t_6_ginkgonut_18_i@yahoo.co.jp>
2018-09-07 11:36:12 +0900

    change dirctory

--- client/PusherChat/app/src/main/java/com/example/toshiki/pusherchat/MainActivity.kt
+++ client/PusherChat/app/src/main/java/com/example/toshiki/pusherchat/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.toshiki.pusherchat
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
