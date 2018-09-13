9de309324ad2848e063dd4b101881c2e319e7738
proelbtn <proelbtn@users.noreply.github.com>
2018-09-14 01:29:43 +0900

    [ADD] スプラッシュ画面を追加した。

--- frontend/app/src/main/java/com/proelbtn/linesc/activities/MainActivity.kt
+++ frontend/app/src/main/java/com/proelbtn/linesc/activities/MainActivity.kt
@@ -1,12 +1,14 @@
-package com.proelbtn.linesc
+package com.proelbtn.linesc.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.proelbtn.linesc.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setTheme(R.style.AppTheme)
+        setContentView(R.layout.main)
     }
 }
