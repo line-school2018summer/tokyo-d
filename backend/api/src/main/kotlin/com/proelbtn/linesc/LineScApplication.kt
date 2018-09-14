1c1a6cd484f044a45a12563d866e113b5dcaab1e
proelbtn <proelbtn@users.noreply.github.com>
2018-09-14 18:22:08 +0900

    [ADD] デフォルトで生成されるレスポンスを消した。

--- backend/api/src/main/kotlin/com/proelbtn/linesc/LineScApplication.kt
+++ backend/api/src/main/kotlin/com/proelbtn/linesc/LineScApplication.kt
@@ -38,6 +38,7 @@ class LineScApplication {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
+                .useDefaultResponseMessages(false)
                 .apiInfo(info)
     }
 
