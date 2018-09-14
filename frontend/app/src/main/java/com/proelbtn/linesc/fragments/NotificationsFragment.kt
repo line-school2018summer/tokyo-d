6b2e1a1e01eceb00835cab03ed4b7e5de8b77532
proelbtn <proelbtn@users.noreply.github.com>
2018-09-14 15:59:17 +0900

    [ADD] NavigationViewを用いた遷移を実装した。

---
+++ frontend/app/src/main/java/com/proelbtn/linesc/fragments/NotificationsFragment.kt
@@ -0,0 +1,22 @@
+package com.proelbtn.linesc.fragments
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.proelbtn.linesc.R
+
+class NotificationsFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.notifications, container, false)
+    }
+}
--- frontend/app/src/main/java/com/proelbtn/linesc/presenters/MainPresenter.kt
+++ frontend/app/src/main/java/com/proelbtn/linesc/presenters/MainPresenter.kt
@@ -1,10 +1,21 @@
 package com.proelbtn.linesc.presenters
 
 class MainPresenter (val view: View) {
+    fun selectHome() {
+        view.showHome()
+    }
 
+    fun selectDashboard() {
+        view.showDashboard()
+    }
 
+    fun selectNotifications() {
+        view.showNotifications()
+    }
 
     interface View {
-
+        fun showHome()
+        fun showDashboard()
+        fun showNotifications()
     }
 }
