7cd90c8612283c020bebd05d9de1a1f7f48aa525
ti-ginkgo <t_6_ginkgonut_18_i@yahoo.co.jp>
2018-09-23 23:50:00 +0900

    implementation login

--- frontend/app/src/main/java/com/proelbtn/linesc/presenters/LoginPresenter.kt
+++ frontend/app/src/main/java/com/proelbtn/linesc/presenters/LoginPresenter.kt
@@ -1,17 +1,37 @@
 package com.proelbtn.linesc.presenters
 
 import android.content.Context
+import android.widget.Toast
 import com.proelbtn.linesc.managers.StoredDataManager
+import com.proelbtn.linesc.models.datainterface.SearchUsersGet
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class LoginPresenter (val view: View) {
     fun onLogin() {
         val sid = view.getSid()
         val pass = view.getPassword()
 
-        StoredDataManager.setSid(sid)
-        StoredDataManager.setPass(pass)
+        if (sid.isEmpty() or pass.isEmpty()) {
+            Toast.makeText(view.getContext(),"Username and Password should not be empty", Toast.LENGTH_SHORT).show()
 
-        view.navigateToMainActivity()
+        } else {
+            SearchUsersGet.create()
+                    .getSearchUsers(sid)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe (
+                            {
+                                StoredDataManager.setSid(sid)
+                                StoredDataManager.setPass(pass)
+
+                                view.navigateToMainActivity()
+                            },
+                            {
+                                Toast.makeText(view.getContext(),"Username or Password is wrong", Toast.LENGTH_SHORT).show()
+                            }
+                    )
+        }
     }
 
     interface View {
